9fb94ea48ded7157028b01e29cc81a75a599c265
adim <wjx@djl.com>
2024-04-24 16:08:14 +0800

    第一次提交

---
+++ src/main/java/com/csdj/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.csdj.controller;
+
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("hello,git1");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
+        System.out.println("hello,git4");
+        System.out.println("master,test11dd");
+
+    }
+}
