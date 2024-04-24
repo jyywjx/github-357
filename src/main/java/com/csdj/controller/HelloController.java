795a716f8fa4f591525c8bc61f1b372564f2cfc5
adim <wjx@djl.com>
2024-04-24 16:35:18 +0800

    第2次提交

--- src/main/java/com/csdj/controller/HelloController.java
+++ src/main/java/com/csdj/controller/HelloController.java
@@ -6,8 +6,7 @@ public class HelloController {
         System.out.println("hello,git2");
         System.out.println("hello,git3");
         System.out.println("hello,git4");
-        System.out.println("master,test11dd");
-        System.out.println("master,test");
+        System.out.println("master,test12");
 
     }
 }
